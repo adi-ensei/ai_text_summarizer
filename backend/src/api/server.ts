1afcc1db1e886e4fc651569612a644551937843a
adi-ensei <your_github_email@example.com>
2025-10-03 23:57:24 +0530

    debugged for deployment

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import summarizeRouter from "./routes/summarize";
+import summarizeRouter from "../routes/summarize";
 
 dotenv.config();
 
